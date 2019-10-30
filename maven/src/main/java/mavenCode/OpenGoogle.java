590f0dae778c815cefaa6c112638ec8f8c4a1bac
Pradeep Sekar <pradeepsek@gmail.com>
2019-10-30 15:23:12 -0400

    commit maven

---
+++ maven/src/main/java/mavenCode/OpenGoogle.java
@@ -0,0 +1,15 @@
+package mavenCode;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle 
+{
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\prade\\Documents\\Keerthana\\Eclipse\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+	}
+	
+}
